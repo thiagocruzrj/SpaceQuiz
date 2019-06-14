702779ab6a61dd21cd60e5e370143f3db6b6935e
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-06-14 20:06:26 -0300

    85% completed

--- app/src/main/java/com/study/spacequiz/ResultActivity.java
+++ app/src/main/java/com/study/spacequiz/ResultActivity.java
@@ -1,4 +1,34 @@
 package com.study.spacequiz;
 
-public class ResultActivity {
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        TextView resultAnswers = findViewById(R.id.resultAnswers);
+        MediaPlayer win;
+        MediaPlayer fail;
+        int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
+
+        win = MediaPlayer.create(this, R.raw.nice);
+        fail = MediaPlayer.create(this, R.raw.lose);
+
+        if (score >=4 ) {
+            win.start();
+        } else {
+            fail.start();
+        }
+    }
+    public void returnInitial(View view){
+        Intent intent = new Intent(getApplicationContext(), InitialActivity.class);
+        startActivity(intent);
+    }
 }
